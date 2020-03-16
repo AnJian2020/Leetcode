8b4cd089c2fd21f4843e8f4171908b2deb7185f4
AnJian2020 <xuhao2018@foxmail.com>
2020-03-16 11:00:25 +0800

    邀请码检测

--- leetcode/邀请码检测.py
+++ leetcode/邀请码检测.py
@@ -13,12 +13,14 @@ class Solution:
         evenNumberSum,oddNumberSum,item = 0,0,invitationCodeLength-1
         #evenNumberSum为偶数位和,oddNumberSum为奇数位和
         while item >= 0:
-            if (item+1)%2 == 1:     #奇数和
+            if (item+1)%2 == 1:
+                #奇数和
                 if invitationCode[item].isdigit():
                     oddNumberSum+=int(invitationCode[item])
                 else:
                     oddNumberSum+=ord(invitationCode[item])%96%9
-            elif (item+1)%2 == 0:       #偶数和
+            elif (item+1)%2 == 0:
+                #偶数和
                 if invitationCode[item].isdigit():
                     oddSum = int(invitationCode[item])*2
                 else:
